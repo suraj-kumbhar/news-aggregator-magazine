ffb1f2ff598b72763753db0aef67c8b1193547a8
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-03-30 16:13:10 +0530

    Updates

---
+++ precache-manifest.5794eb644be4c405b7855e32a29514a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f075f4e145e42d954f9a414327305268",
+    "url": "/news-aggregator-magazine/index.html"
+  },
+  {
+    "revision": "dff65ac62a6540c68071",
+    "url": "/news-aggregator-magazine/static/css/main.8c95f42a.chunk.css"
+  },
+  {
+    "revision": "f9379ff90f29c5bb3cd9",
+    "url": "/news-aggregator-magazine/static/js/2.a1e765cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news-aggregator-magazine/static/js/2.a1e765cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff65ac62a6540c68071",
+    "url": "/news-aggregator-magazine/static/js/main.ef44248e.chunk.js"
+  },
+  {
+    "revision": "5b5697f3d41f229c897d",
+    "url": "/news-aggregator-magazine/static/js/runtime-main.19ccc8e0.js"
+  }
+]);
